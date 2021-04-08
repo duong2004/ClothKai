c6231b79020efdc57a805605c3a82888cfd1bce7
HTDCoders <huynhtienduong.huynh@gmail.com>
2021-04-08 21:32:16 +0700

    CRUD Category Operations in Entity Framework with ASP .Net MVC

---
+++ ClothKai.Services/CategoryService.cs
@@ -0,0 +1,58 @@
+﻿using ClothKai.Database;
+using ClothKai.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothKai.Services
+{
+    public class CategoryService
+    {
+        // Get All Category
+        public List<Category> GetCategory()
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        // Save Category
+        public void SaveCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+        // Get Category to ID
+        public Category GetCategoryID(int ID)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        // Update Category
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        // Delete Category
+        public void DeleteCategory(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                var Category = context.Categories.Find(category.ID);
+                context.Categories.Remove(Category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothKai.Web/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using ClothKai.Entities;
+using ClothKai.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothKai.Web.Controllers
+{   
+    public class CategoryController : Controller
+    {
+        CategoryService categoryService = new CategoryService();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var listCategory = categoryService.GetCategory();
+            return View(listCategory);
+        }
+        // GET: Category/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var categoryID = categoryService.GetCategoryID(ID);
+            if (categoryID == null)
+            {
+                return View("Error");
+            }
+            return View(categoryID);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var categoryID = categoryService.GetCategoryID(ID);
+            if (categoryID == null)
+            {
+                return View("Error");
+            }
+            return View(categoryID);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoryService.DeleteCategory(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
