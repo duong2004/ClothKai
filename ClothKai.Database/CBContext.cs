004c75d5a16e09cd0e858d3af7353fe6f25e7d16
HTDCoders <huynhtienduong.huynh@gmail.com>
2021-04-13 09:37:30 +0700

    Custom URLs and Application Configurations in ASP .Net MVC

--- ClothKai.Database/CBContext.cs
+++ ClothKai.Database/CBContext.cs
@@ -16,5 +16,6 @@ namespace ClothKai.Database
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Config> Configurations { get; set; }
     }
 }
---
+++ ClothKai.Services/ConfigrutionService.cs
@@ -0,0 +1,21 @@
+﻿using ClothKai.Database;
+using ClothKai.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothKai.Services
+{
+    public class ConfigrutionService
+    {
+        public Config GetConfig(string Key)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Configurations.Find(Key);
+            }
+        }
+    }
+}
